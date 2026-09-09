3b7e7d6ab458a2175376adb9a0a300c94a48d32f
Power Trip <gorzan0123@gmail.com>
2026-09-09 14:54:19 +0400

    Update walkie.ts

--- walkie.ts
+++ walkie.ts
@@ -4,7 +4,14 @@ namespace walkieTalkie {
     //% block="initialize walkie talkie"
     //% shim=WalkieTalkie::init
     export function init(): void {
-        return; // Left blank, MakeCode shims this straight to C++
+        return; 
+    }
+
+    //% block="set walkie talkie group to %group"
+    //% group.min=0 group.max=255 group.defl=1
+    //% shim=WalkieTalkie::setRadioGroup
+    export function setRadioGroup(group: number): void {
+        return;
     }
 
     //% block="start recording audio"
