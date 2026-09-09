4fee14fbfed4b32798d90b91581ba3bcdfa2892e
Power Trip <gorzan0123@gmail.com>
2026-09-09 15:05:20 +0400

    Update walkie.ts

--- walkie.ts
+++ walkie.ts
@@ -1,7 +1,11 @@
 //% color="#E67E22" icon="\uf130" block="Walkie Talkie"
 namespace walkieTalkie {
 
-    //% block="initialize walkie talkie with sample rate %sampleRate Hz"
+    /**
+     * Initialize walkie talkie with a completely custom sample rate in Hz.
+     * @param sampleRate Choose any rate (e.g., 4000 to 11000), default: 8000
+     */
+    //% block="initialize walkie talkie with sample rate $sampleRate Hz"
     //% sampleRate.defl=8000
     //% shim=WalkieTalkie::init
     export function init(sampleRate: number): void {
