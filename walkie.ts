e7b079b11b5cd4b76c3602b28b801f35c0026060
Power Trip <gorzan0123@gmail.com>
2026-09-09 14:49:04 +0400

    Create walkie.ts

---
+++ walkie.ts
@@ -0,0 +1,21 @@
+//% color="#E67E22" icon="\uf130" block="Walkie Talkie"
+namespace walkieTalkie {
+
+    //% block="initialize walkie talkie"
+    //% shim=WalkieTalkie::init
+    export function init(): void {
+        return; // Left blank, MakeCode shims this straight to C++
+    }
+
+    //% block="start recording audio"
+    //% shim=WalkieTalkie::startRecording
+    export function startRecording(): void {
+        return;
+    }
+
+    //% block="transmit recorded audio"
+    //% shim=WalkieTalkie::transmitAudio
+    export function transmitAudio(): void {
+        return;
+    }
+}
